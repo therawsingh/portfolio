ef478392e6327128a6fd9dce22eac80b47d6c8c3
fLuKeroshan <32307382+fLuKeroshan@users.noreply.github.com>
2023-03-17 22:15:12 +0530

    Initial Commit

---
+++ src/main/java/com/therawsingh/portfolio/TherawsinghPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.therawsingh.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherawsinghPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherawsinghPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/therawsingh/portfolio/TherawsinghPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therawsingh.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherawsinghPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
